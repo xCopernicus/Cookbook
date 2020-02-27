46f2aa00cb41ecd39a5dff668e12deb23c17006a
Franek Zawada <franek.zawada@gmail.com>
2020-02-27 12:52:30 +0100

    Add adding active class to clicked element

--- js/script.js
+++ js/script.js
@@ -3,20 +3,26 @@
 const links = document.querySelectorAll('.titles a');
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
   console.log(event);
-  /* remove class 'active' from all article links  */
+
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
-//  link.classList.add('active')
-  /* remove class 'active' from all articles */
+
+  /* [DONE] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.posts article.active');
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
+
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
