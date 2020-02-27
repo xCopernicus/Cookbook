1b353fe834a850738e4209fbbab523bae222641b
Franek Zawada <franek.zawada@gmail.com>
2020-02-27 12:40:59 +0100

    Add 2 loops for removing active class

--- js/script.js
+++ js/script.js
@@ -5,14 +5,18 @@ const links = document.querySelectorAll('.titles a');
 function titleClickHandler(event){
   console.log('Link was clicked!');
   console.log(event);
-
-
   /* remove class 'active' from all article links  */
-
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
   /* add class 'active' to the clicked link */
-
+//  link.classList.add('active')
   /* remove class 'active' from all articles */
-
+  const activeArticles = document.querySelectorAll('.posts article.active');
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
